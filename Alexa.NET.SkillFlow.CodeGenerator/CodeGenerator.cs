62a337f6061014cf2f86a611f5b5951a83cf3612
Steven Pears <steven.pears@live.co.uk>
2019-10-27 11:27:45 +0000

    scene begin

--- Alexa.NET.SkillFlow.CodeGenerator/CodeGenerator.cs
+++ Alexa.NET.SkillFlow.CodeGenerator/CodeGenerator.cs
@@ -4,19 +4,27 @@ using Alexa.NET.SkillFlow.Generator;
 
 namespace Alexa.NET.SkillFlow.CodeGenerator
 {
-    public class CodeGenerator:SkillFlowGenerator<CodeGeneratorContext>
+    public class CodeGenerator : SkillFlowGenerator<CodeGeneratorContext>
     {
         protected override Task Begin(Scene scene, CodeGeneratorContext context)
         {
             var code = new CodeCompileUnit();
             var ns = new CodeNamespace("SkillFlow");
             code.Namespaces.Add(ns);
-            
+
             ns.Imports.Add(new CodeNamespaceImport("Alexa.NET.RequestHandlers"));
             ns.Imports.Add(new CodeNamespaceImport("Alexa.NET.APL"));
 
+            var mainClass = new CodeTypeDeclaration(SceneClassName(scene.Name)) { IsClass = true };
+            ns.Types.Add(mainClass);
+
             context.CodeFiles.Add(scene.Name, code);
             return base.Begin(scene, context);
+        }
+
+        private string SceneClassName(string sceneName)
+        {
+            return "Scene_" + char.ToUpper(sceneName[0]) + sceneName.Substring(1).Replace(" ", "_");
         }
     }
 }
